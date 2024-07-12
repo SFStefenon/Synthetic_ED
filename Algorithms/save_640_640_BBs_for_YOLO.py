84bc27dc40e02e01599a80c92b50ca4031b0a89a
Stefano F. Stefenon <88292916+SFStefenon@users.noreply.github.com>
2024-07-12 14:50:55 +0200

    Create save_640_640_BBs_for_YOLO.py

---
+++ Algorithms/save_640_640_BBs_for_YOLO.py
@@ -0,0 +1,43 @@
+import glob
+
+main = str('./Synthetic/Final')
+# Clean the processing folder
+import os
+for f in glob.glob(main+"/Cropouts/S*.txt"):
+    os.remove(f)
+
+# Load the files
+def get_file_names(path):
+    ext = ['txt']    # Add formats here
+    files = []
+    [files.extend(glob.glob(path + 'S*.' + e)) for e in ext]
+    return files
+files = get_file_names(main+'/Original/')
+
+for file in files:
+    with open(file=file) as f:
+        lines = f.readlines()
+    for line in lines:
+        for slide_ln in range(0,9):
+            new_lines = []
+
+            if int(line[-27])==0:
+                if int(slide_ln)==int(line[-36]):
+                    final_line = line.replace((' ' + str(slide_ln)),' 0', 1)
+                    new_lines.append(final_line)
+                    new_file = 'Synthetic/Final/Cropouts/' + file[-20:-4] + '_R_0_' + str(int(line[-36])) + '.txt'
+                    g = open(new_file, "a")
+                    g.writelines(new_lines)
+                    print(new_file)
+
+            if int(line[-27])==1:
+                if int(slide_ln)==int(line[-36]):
+                    final_line = line.replace((' ' + str(1)),' 0', 2)
+                    new_lines.append(final_line)
+                    new_file = 'Synthetic/Final/Cropouts/' + file[-20:-4] + '_R_1_' + str(int(line[-36])) + '.txt'
+                    g = open(new_file, "a")
+                    g.writelines(new_lines)
+                    print(new_file)
+
+g.close()
+     
