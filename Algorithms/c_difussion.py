f501672b87258410abc9e4d591ed35dba7fc2504
Stefano F. Stefenon <88292916+SFStefenon@users.noreply.github.com>
2024-07-12 15:27:16 +0200

    Update c_difussion.py

--- Algorithms/c_difussion.py
+++ Algorithms/c_difussion.py
@@ -16,7 +16,7 @@ print('device:', device)
 
 # Define the storage and dataset path
 save_dir = './saved_results/'
-train_data_path = '28by28_rfi_train_perfect_labels_0.csv'
+train_data_path = '28by28_rfi_train_perfect_labels_1.csv'
 
 n_epoch = 10 # 5000
 samples = 10
